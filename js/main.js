373a8bebdcb128ead2efcb1be062e480c65c666c
tairhiq <tairhiqharris@yahoo.com>
2024-10-08 09:08:11 -0500

    java script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // JS scripts placed here
+const myButton = document.getElementById("intro");
+const myButton2 = document.getElementById("submit");
+console.log(myButton);
+console.log(myButton2);
+
+const element = document.getElementsByTagName("p");
+console. log(element);
+
+const y = document.querySelector(".logo")
+consol.log(y);
